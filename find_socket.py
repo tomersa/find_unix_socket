79f20b3f74c9e1b6ac8d38e0e132339a5529689e
Tomer Saban <sab30226@gmail.com>
2021-11-27 01:44:59 +0200

    Some type hinting

--- find_socket.py
+++ find_socket.py
@@ -6,14 +6,14 @@ import glob
 assert len(sys.argv) == 2, f'Usage: {sys.argv[0]} <socket name>'
 socket_name = sys.argv[1]
 
-def find_socket_inode():
+def find_socket_inode() -> None:
     with open('/proc/net/unix') as f:
         for line in f.readlines():
             if socket_name in line:
                 return line.split(' ')[6]
     raise Exception(f'Couldnt find inode associated with socket {socket_name}.')
 
-def find_proccess_by_inode(socket_inode: str):
+def find_proccess_by_inode(socket_inode: str) -> None:
     for f in glob.glob('/proc/[0-9]*/fd/*', recursive=False):
         try:
             if socket_inode in os.readlink(f):
